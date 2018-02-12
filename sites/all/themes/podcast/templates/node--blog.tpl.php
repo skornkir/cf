30d2d68f4f8701046074f09218a8068d993aa665
Sancheclaus <vip.lisicin@mail.ru>
2018-02-13 02:56:25 +0300

    Верстка записи блога

--- sites/all/themes/podcast/templates/node--blog.tpl.php
+++ sites/all/themes/podcast/templates/node--blog.tpl.php
@@ -79,46 +79,20 @@
  * @ingroup themeable
  */
 ?>
-new template
+<div class="title"><?php print $title?>
+  <div class="line"></div>
+</div>
 <article class="<?php print $classes; ?> clearfix node-<?php print $node->nid; ?>"<?php print $attributes; ?>>
-    <?php print $created; ?>
-    <?php dpm($content); ?>
-    <?php print $content['body'][0]['#markup']; ?>
-    <div>
-        <?php print date("d.m.y" , $created); ?>
-    </div>
     <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || $preview || !$page && $title): ?>
         <header>
-            <?php print render($title_prefix); ?>
-            <?php if (!$page && $title): ?>
-                <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
-
-            <?php if ($display_submitted): ?>
-                <p class="submitted">
-                    <?php print $user_picture; ?>
-                    <?php print $submitted; ?>
-                </p>
-            <?php endif; ?>
-
-            <?php if ($unpublished): ?>
-                <mark class="watermark"><?php print t('Unpublished'); ?></mark>
-            <?php elseif ($preview): ?>
-                <mark class="watermark"><?php print t('Preview'); ?></mark>
-            <?php endif; ?>
+            <div class="DAticle">
+                <div class="RTxt">Опубликовано: </div> <?php print date("d.m.Y" , $created);?>
+            </div>
         </header>
+    <div class="TxtAticle">
     <?php endif; ?>
-
     <?php
-    // We hide the comments and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    print render($content);
+        print render($content);
     ?>
-
-    <?php print render($content['links']); ?>
-
-    <?php print render($content['comments']); ?>
-
+        </div>
 </article>
